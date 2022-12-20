ebb6393c96df6171e7bccd85e52bcc5808df8fbd
Aldo Navarrete <aaldiitoo@gmail.com>
2022-12-20 16:19:34 -0600

    Initial Commit

---
+++ realestateapp/realestateappApp.swift
@@ -0,0 +1,17 @@
+//
+//  realestateappApp.swift
+//  realestateapp
+//
+//  Created by Aldo Navarrete on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct realestateappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
